be90bef331f77e998ff2c348a4d5b65e81c50377
Jake(Junya) Sewai <jsewai@JakeJunyas-MacBook-Pro.local>
2021-08-05 16:54:14 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const app = new Vue({
+    el: '.app',
+    data: {
+        notes: [],
+        currentNote: null
+    },
+    mounted(){
+        if (localStorage.notes){
+            this.notes = JSON.parse(localStorage.notes);
+        }
+    },
+    watch: {
+        notes: {
+            handler(newNotes){
+                localStorage.notes = JSON.stringify(newNotes);
+            },
+            deep: true
+        }
+    },
+    methods: {
+        createNote(){
+            const newNote = {title: '', contents: ''};
+            this.notes.push(newNote);
+            this.currentNote = newNote;
+            this.$nextTick(function(){
+                this.$refs.noteTitle.focus();
+            })
+        }
+    }
+})
